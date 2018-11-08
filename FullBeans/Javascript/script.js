1099b1a1c9fb8b23af38e6198c78870aa83fee68
Harrison Bennett <32133987+HMSBennett@users.noreply.github.com>
2018-11-08 18:43:40 +0000

    Updated Full Beanz

---
+++ FullBeans/Javascript/script.js
@@ -0,0 +1,15 @@
+function openHome(){
+  document.getElementById('HomeAnchor').scrollIntoView();
+}
+
+function openTeam(){
+  document.getElementById('TeamAnchor').scrollIntoView();
+}
+
+function openProject(){
+  document.getElementById('ProjectAnchor').scrollIntoView();
+}
+
+function openPaper(){
+  document.getElementById('PaperAnchor').scrollIntoView();
+}
