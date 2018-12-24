db4d566f0dc98445cab6a7988406becaed38a392
Harrison Bennett <32133987+HMSBennett@users.noreply.github.com>
2018-12-24 20:33:46 +0000

    All data comes from the server now

--- HMS Bennett/Tali/Javascript/main.js
+++ HMS Bennett/Tali/Javascript/main.js
@@ -1,47 +1,134 @@
-var itemsJSON = [{
-    "title":"Tali\'s Textiles - 001",
-    "name":"Tali Zorah Patterned Scarf",
-    "cost":"&pound" + "12.50",
-    "quantity":"Quantity Left: 7",
-    "description":"This scarf connects end to end and is great for the cold weather",
-    "imageSrc":"images/TaliScarf.png"
-  },{
-    "title":"Tali\'s Textiles - 002",
-    "name":"Leather N7 Jacket",
-    "cost":"&pound" + "37.50",
-    "quantity":"Quantity Left: 5",
-    "description":"This faux leather jacket lets everyone know your favourite store on the citadel",
-    "imageSrc":"images/N7Jacket.png"
-  },{
-    "title":"Tali\'s Textiles - 003",
-    "name":"Krogan \'Lift\' T-Shirt",
-    "cost":"&pound" + "12.50",
-    "quantity":"Quantity Left: 2",
-    "description":"Wear at the gym to let everyone know, you lift like a Krogan",
-    "imageSrc":"images/kroganTshirt.png"
-  },{
-    "title":"Tali\'s Textiles - 004",
-    "name":"Wooden N7 Eagle Pistol",
-    "cost":"&pound" + "25.50",
-    "quantity":"Quantity Left: 1",
-    "description":"A wooden replica of Shepards trusty pistol",
-    "imageSrc":"images/N7Eagle.png"
-}];
-
-var itemClickedNo = 1;
+var itemsArray = [];
+var itemsTitle = [];
+var itemsName = [];
+var itemsCost = [];
+var itemsQuantity = [];
+var itemsDescription = [];
+var itemsImages = [];
+var totalItems = 4;
+var itemNumber = 0;
+var itemClickedNo = 0;
 var cIndex = 0;
 var rIndex = 0;
 
 function OnLoad(){
-  // console.log("Test: " + test);
-
+  //decides how to proceed depending on what page is open
   if(document.getElementById('pageTitle').innerHTML == "Tali's Textiles - Home"){
+    GetTotalItems();
+    GetHomeItems();
     PopulateHomePage();
-  }else{
+  }else if(document.getElementById('pageTitle').innerHTML == "Tali's Textiles - Basket"){
+    // UpdateBasket();
+  }else if(document.getElementById('titleData').innerHTML == "Tali's Textiles - "){
     itemClickedNo = localStorage.getItem("itemClicked");
-    LoadPageDetails(itemClickedNo);
+    GetPageItems();
+    PopulateItemPage();
+  }else{
+
   }
 }
+
+//Database Call-----------------------------------------------------------------
+
+function GetTotalItems(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      totalItems = this.responseText;
+    }
+  };
+  xhttp.open("GET", "http://localhost:9090/totalItems", false);
+  xhttp.send();
+  return;
+}
+
+function GetHomeItems(){
+  GetItemName();
+  GetItemImageSrc();
+}
+
+function GetPageItems(){
+  GetItemTitle();
+  GetItemName();
+  GetItemCost();
+  GetItemQuantity();
+  GetItemDescription();
+  GetItemImageSrc();
+}
+
+function GetItemTitle(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var string = this.responseText;
+      itemsTitle = string.split(",");
+    }
+  };
+  xhttp.open("GET", "http://localhost:9090/itemTitle", false);
+  xhttp.send();
+}
+
+function GetItemName(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var string = this.responseText;
+      itemsName = string.split(",");
+    }
+  };
+  xhttp.open("GET", "http://localhost:9090/itemName", false);
+  xhttp.send();
+}
+
+function GetItemCost(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var string = this.responseText;
+      itemsCost = string.split(",");
+    }
+  };
+  xhttp.open("GET", "http://localhost:9090/itemCost", false);
+  xhttp.send();
+}
+
+function GetItemQuantity(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var string = this.responseText;
+      itemsQuantity = string.split(",");
+    }
+  };
+  xhttp.open("GET", "http://localhost:9090/itemQuantity", false);
+  xhttp.send();
+}
+
+function GetItemDescription(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var string = this.responseText;
+      itemsDescription = string.split(",");
+    }
+  };
+  xhttp.open("GET", "http://localhost:9090/itemDescription", false);
+  xhttp.send();
+}
+
+function GetItemImageSrc(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var string = this.responseText;
+      itemsImages = string.split(",");
+    }
+  };
+  xhttp.open("GET", "http://localhost:9090/itemImageSrc", false);
+  xhttp.send();
+}
+
+//Home Page Code----------------------------------------------------------------
 
 function storeClick(item){
   // console.log(item.cellIndex);
@@ -57,6 +144,7 @@ function storeRowClick(item){
   rIndex = item.rowIndex;
   var output = 0;
 
+  //calculates which item was picked via C and R index
   if(rIndex == 0){
     output = cIndex;
   }else{
@@ -67,13 +155,12 @@ function storeRowClick(item){
 }
 
 function PopulateHomePage(){
-  var totalBoxes = 0;
   var table = document.getElementById('menuContent');
-  itemsJSON.forEach(function(obj){
-    totalBoxes++;
-  });
-  var rowsNeeded = Math.ceil(totalBoxes / 3);
-  var remainder = totalBoxes % 3;
+
+  var rowsNeeded = Math.ceil(totalItems / 3);
+  var remainder = totalItems % 3;
+  console.log(totalItems + " - " + rowsNeeded + " - " + remainder);
+
   for(var i = 0; i < rowsNeeded; i++){
     console.log("Remainder = " + remainder + " Rows Needed = " + rowsNeeded);
     var row = table.insertRow(i);
@@ -82,40 +169,94 @@ function PopulateHomePage(){
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     if(remainder == 1 && i == (rowsNeeded - 1)){
-      cell1.innerHTML = "<a href='TaliItem.html'>" + itemsJSON[i + 2].name + "<br><br> <img src='" + itemsJSON[i + 2].imageSrc + "' width='75%'> </a>";
+      cell1.innerHTML = "<a href='TaliItem.html'>" + itemsName[i + 2] + "<br><br>" + itemsImages[i + 2] + "</a>";
       cell1.setAttribute("onclick", "storeClick(this)");
       console.log("one row fill");
     }else if(remainder == 2 && i == (rowsNeeded - 1)){
-      cell1.innerHTML = "<a href='TaliItem.html'>" + itemsJSON[i + 2].name + "<br><br> <img src='" + itemsJSON[i + 2].imageSrc + "' width='75%'> </a>";
+      cell1.innerHTML = "<a href='TaliItem.html'>" + itemsName[i + 2] + "<br><br>" + itemsImages[i + 2] + "</a>";
       cell1.setAttribute("onclick", "storeClick(this)");
-      cell2.innerHTML = "<a href='TaliItem.html'>" + itemsJSON[i + 3].name + "<br><br> <img src='" + itemsJSON[i + 3].imageSrc + "' width='75%'> </a>";
+      cell2.innerHTML = "<a href='TaliItem.html'>" + itemsName[i + 3] + "<br><br>" + itemsImages[i + 3] + "</a>";
       cell2.setAttribute("onclick", "storeClick(this)");
       console.log("two row fill");
     }else if (i == 0){
-      cell1.innerHTML = "<a href='TaliItem.html'>" + itemsJSON[i].name + "<br><br> <img src='" + itemsJSON[i].imageSrc + "' width='75%'> </a>";
+      cell1.innerHTML = "<a href='TaliItem.html'>" + itemsName[i] + "<br><br>" + itemsImages[i] + "</a>";
       cell1.setAttribute("onclick", "storeClick(this)");
-      cell2.innerHTML = "<a href='TaliItem.html'>" + itemsJSON[i + 1].name + "<br><br> <img src='" + itemsJSON[i + 1].imageSrc + "' width='75%'> </a>";
+      cell2.innerHTML = "<a href='TaliItem.html'>" + itemsName[i + 1] + "<br><br>" + itemsImages[i + 1] + "</a>";
       cell2.setAttribute("onclick", "storeClick(this)");
-      cell3.innerHTML = "<a href='TaliItem.html'>" + itemsJSON[i + 2].name + "<br><br> <img src='" + itemsJSON[i + 2].imageSrc + "' width='75%'> </a>";
+      cell3.innerHTML = "<a href='TaliItem.html'>" + itemsName[i + 2] + "<br><br>" + itemsImages[i + 2] + "</a>";
       cell3.setAttribute("onclick", "storeClick(this)");
       console.log("start row fill");
     }else{
-      cell1.innerHTML = "<a href='TaliItem.html'>" + itemsJSON[i + 2].name + "<br><br> <img src='" + itemsJSON[i + 2].imageSrc + "' width='75%'> </a>";
+      cell1.innerHTML = "<a href='TaliItem.html'>" + itemsName[i + 2] + "<br><br>" + itemsImages[i + 2] + "</a>";
       cell1.setAttribute("onclick", "storeClick(this)");
-      cell2.innerHTML = "<a href='TaliItem.html'>" + itemsJSON[i + 3].name + "<br><br> <img src='" + itemsJSON[i + 3].imageSrc + "' width='75%'> </a>";
+      cell2.innerHTML = "<a href='TaliItem.html'>" + itemsName[i + 3] + "<br><br>" + itemsImages[i + 3] + "</a>";
       cell2.setAttribute("onclick", "storeClick(this)");
-      cell3.innerHTML = "<a href='TaliItem.html'>" + itemsJSON[i + 4].name + "<br><br> <img src='" + itemsJSON[i + 4].imageSrc + "' width='75%'> </a>";
+      cell3.innerHTML = "<a href='TaliItem.html'>" + itemsName[i + 4] + "<br><br>" + itemsImages[i + 4] + "</a>";
       cell3.setAttribute("onclick", "storeClick(this)");
       console.log("three row fill");
     }
   }
 }
 
-function LoadPageDetails(itemClickedNo){
-  document.getElementById('pageTitle').innerHTML = itemsJSON[itemClickedNo].title;
-  document.getElementById('itemName').innerHTML = itemsJSON[itemClickedNo].name;
-  document.getElementById('itemCost').innerHTML = itemsJSON[itemClickedNo].cost;
-  document.getElementById('itemQuantity').innerHTML = itemsJSON[itemClickedNo].quantity;
-  document.getElementById('itemDescription').innerHTML = itemsJSON[itemClickedNo].description;
-  document.getElementById('itemImage').src = itemsJSON[itemClickedNo].imageSrc;
+//Item Page Code----------------------------------------------------------------
+
+function PopulateItemPage(){
+  document.getElementById('titleData').innerHTML = itemsTitle[itemClickedNo];
+  document.getElementById('nameData').innerHTML = itemsName[itemClickedNo];
+  document.getElementById('costData').innerHTML = "£" + itemsCost[itemClickedNo];
+  document.getElementById('quantityData').innerHTML = "Stock Left: " + itemsQuantity[itemClickedNo];
+  document.getElementById('descriptionData').innerHTML = itemsDescription[itemClickedNo];
+  document.getElementById('imageData').innerHTML = itemsImages[itemClickedNo];
+}
+
+//Basket Code-------------------------------------------------------------------
+
+function AddToBasket(){
+  // itemClickedNo = localStorage.getItem("itemClicked");
+  // var temp = itemsJSON[itemClickedNo].inBasket;
+  // var temp2 = (parseInt(temp) + 1).toString();
+  // itemsJSON[itemClickedNo].inBasket = temp2;
+  // console.log(temp + "  " + temp2);
+}
+
+function ItemsInBasket(){
+// for(int i = 0; i < 0; i++){
+//
+// }
+}
+
+function UpdateBasket(){
+  // for(var i = 0; i < itemsJSON.length; i++){
+  //   if(itemsJSON[i].inBasket != 0){
+  //     document.getElementById('itemName').innerHTML = itemsJSON[i].name;
+  //     document.getElementById('itemCost').innerHTML = itemsJSON[i].cost;
+  //     document.getElementById('itemInBasket').innerHTML = itemsJSON[i].inBasket;
+  //   }else{
+  //     document.getElementById('itemName').innerHTML = "Basket Empty";
+  //   }
+  // }
+}
+
+//Request Code------------------------------------------------------------------
+
+function SendRequest(){
+  var name = document.getElementById('requestName').value;
+  var email = document.getElementById('requestEmail').value;
+  var description = document.getElementById('requestNature').value;
+  var url = document.getElementById('requestURL').value;
+
+  if(name == "" || email == "" || description == "" || url == ""){
+    alert("Please fill out all areas to the best of your ability, as this will help the design process of your request.");
+  }else{
+    // alert("Name: " + name + " Email: " + email + " Des: " + description + " URL: " + url);
+    var bodyMessage = "Name: " + name + " Email: " + email + " Des: " + description + " URL: " + url;
+    var subject = "Tali's Textiles Request"
+
+    var mailtoLink = "mailto:somebody@example.com" + "?subject" + subject + "&body=" + bodyMessage;
+
+    emailWindow = window.open(mailtoLink);
+    // emailWindow.close();
+
+    // window.href("mailto:someone@example.com");
+  }
 }
---
+++ HMS Bennett/Tali/Javascript/server.js
@@ -0,0 +1,72 @@
+//Access Database---------------------------------------------------------------
+
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb+srv://bazzy:bazzyb@clientside-qre9x.mongodb.net/test?retryWrites=true";
+
+var itemTitle = [];
+var itemName = [];
+var itemCost = [];
+var itemQuantity = [];
+var itemDescription = [];
+var itemImageSrc = [];
+var totalItems = 0;
+
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    throw err;
+  }
+  console.log("Database Accessable");
+  var dbo = db.db("stock");
+
+  dbo.collection("stockCount").find({}).toArray(function(err, result) {
+    if (err) {
+      throw err;
+    }
+    for(var i = 0; i < result.length; i++){
+      itemTitle[i] = result[i].title;
+      itemName[i] = result[i].name;
+      itemCost[i] = result[i].cost;
+      itemQuantity[i] = result[i].quantity;
+      itemDescription[i] = result[i].description;
+      itemImageSrc[i] = result[i].imageSrc;
+      totalItems++;
+    }
+  });
+  db.close();
+});
+
+//Database URL------------------------------------------------------------------
+//mongodb+srv://bazzy:bazzyb@clientside-qre9x.mongodb.net/test?retryWrites=true
+
+//Speak To Client---------------------------------------------------------------
+
+var http = require ("http");
+var port = 9090;
+var server;
+
+server = http.createServer (function(request,response) {
+
+response.writeHead(200, {"Access-Control-Allow-Origin":"*"});
+
+  if(request.url == "/itemTitle"){
+    response.end ("" + itemTitle + "");
+  }else if(request.url == "/itemName"){
+    response.end ("" + itemName + "");
+  }else if(request.url == "/itemCost"){
+    response.end ("" + itemCost + "");
+  }else if(request.url == "/itemQuantity"){
+    response.end ("" + itemQuantity + "");
+  }else if(request.url == "/itemDescription"){
+    response.end ("" + itemDescription + "");
+  }else if(request.url == "/itemImageSrc"){
+    response.end ("" + itemImageSrc + "");
+  }else if(request.url == "/totalItems"){
+    response.end ("" + totalItems + "");
+  }else{
+    response.end ("<p> Hello World </p> <p> Path requested :" + request.url + "</p>");
+  }
+});
+
+server.listen(port , function(){
+  console.log("Server listening on port" + port);
+});
